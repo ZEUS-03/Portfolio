5e91d5dc8fc5485f10e1b80d5a051642b0be599e
Gautam Sharma <gautam332000lavanya@gmail.com>
2024-01-24 07:35:29 +0530

    updated work and personal details

--- src/components/helper/constant.jsx
+++ src/components/helper/constant.jsx
@@ -20,16 +20,17 @@ import {
 export const PROJECTS_INFO = [
   {
     heading: "MERN App",
+    title: "Ecommerce Fullstack Application",
+    image: "/assets/chat.jpg",
+    link: "https://github.com/ZEUS-03/E-commerce-App",
+  },
+  {
+    heading: "MERN App",
     title: "Social Media Application",
     image: "/assets/sm.jpg",
     link: "https://github.com/ZEUS-03/MERN-social-media-",
   },
-  {
-    heading: "MERN App",
-    title: "Realtime Video Chat Application",
-    image: "/assets/chat.jpg",
-    link: "https://github.com/ZEUS-03/Video-chat-app",
-  },
+
   {
     heading: "React Application",
     title: "Portfolio Website",
